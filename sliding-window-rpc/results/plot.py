a825d8223b48272a3501cb558bb4c5af27e195b7
leightonw-cb <leighton.wilson@cerebras.net>
2023-05-14 10:57:41 -0700

    add plots for all three sides

--- sliding-window-rpc/results/plot.py
+++ sliding-window-rpc/results/plot.py
@@ -17,9 +17,13 @@ peta = 1.E15
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cs2', help='run on CS-2', default=False, action='store_true')
+parser.add_argument('--side', help='left, middle, or right', type=str, default='left')
 args = parser.parse_args()
 
 cs2 = args.cs2
+side = args.side
+assert(side in {'left', 'middle', 'right'})
+
 
 ####
 
@@ -34,6 +38,7 @@ max_left_cycles   = max(df['left_cycles'])
 max_middle_cycles = max(df['middle_cycles'])
 max_right_cycles  = max(df['right_cycles'])
 max_cycles = max(max_left_cycles, max_middle_cycles, max_right_cycles)
+max_cycles = 2500
 
 max_kernel_width = max(df['kernel_width'])
 
@@ -42,20 +47,20 @@ num_elems = list(dict.fromkeys(df['num_elems'].to_list()))
 
 # Plot bandwidth
 for num_elem in num_elems:
-  left_cycles = df.loc[df['num_elems'] == num_elem, 'left_cycles']
+  cycles = df.loc[df['num_elems'] == num_elem, side + '_cycles']
   kernel_width = df.loc[df['num_elems'] == num_elem, 'kernel_width']
-  plt.plot(kernel_width, left_cycles, '-o', label=num_elem)
+  plt.plot(kernel_width, cycles, '-o', label=num_elem)
 
-plt.legend(bbox_to_anchor=(1.05, 1.05))
+plt.legend(bbox_to_anchor=(1.05, 1.05), title='Num elems')
 plt.grid()
 plt.xlabel('Kernel Width (Number of PEs)')
-plt.ylabel('Left PE Cycles')
+plt.ylabel(side + ' PE Cycles')
 plt.xlim([0, max_kernel_width])
 plt.ylim([0, max_cycles])
 
 if not cs2:
-  plt.title("Left Cycle Count, Simulation")
-  plt.savefig('left_cycle_sim.png', bbox_inches='tight')
+  plt.title(side + ' Cycle Count, Simulation')
+  plt.savefig(side + '_cycle_sim.png', bbox_inches='tight')
 else:
-  plt.title("Left Cycle Count, CS2")
-  plt.savefig('left_cycle_cs2.png', bbox_inches='tight')
+  plt.title(side + ' Cycle Count, CS2')
+  plt.savefig(side + '_cycle_cs2.png', bbox_inches='tight')
